102e2422098fc7433ef22b80732eee69a24e9300
Mário Ferreira <47747880+MRDSF@users.noreply.github.com>
2023-12-07 09:54:56 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){
-    return <h1>Bom dia Maninha<br/><br/>Bora lá começar o dia em grande :)<br/><br/>Nota: Isto foi um desafio do curso!</h1>
+    return <h1>Bom dia Maninha<br/><br/>Bora lá começar o dia em grande :)<br/><br/>Nota: Isto foi um desafio do curso! 😘</h1>
 }
 
 export default Home;
